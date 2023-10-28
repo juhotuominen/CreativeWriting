675d0d24bee7a5f18f520f51405a01429b7235ee
Juho Tuominen <juhonen00@gmail.com>
2023-10-28 17:25:21 +0300

    tasks 2.6-2.10

---
+++ part2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import FilterForm from './components/FilterForm';
+import AddPersonForm from './components/AddPersonForm';
+import Persons from './components/Persons';
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook.`);
+      return;
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <FilterForm filter={filter} handleFilterChange={handleFilterChange} />
+      <h2>Add a New</h2>
+      <AddPersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <h2>Numbers</h2>
+      <Persons filteredPersons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
+
