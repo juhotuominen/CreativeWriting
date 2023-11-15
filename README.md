03f7985afc369ad8a5cae1beed11ffba7a534d16
Juho Tuominen <juhonen00@gmail.com>
2023-11-15 23:22:36 +0200

    task 3.11

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Full Stack Open
-Full Stack course by University of Helsinki
+Full Stack course by University of Helsinki  
+  
+Link to task 3.10: https://puhelinluettelobackend-1q67.onrender.com
--- part3/puhelinluettelo_backend/index.js
+++ part3/puhelinluettelo_backend/index.js
@@ -16,6 +16,7 @@ morgan.token('postData', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
